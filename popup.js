a0a6c8c70b58944ffd67a0f8548b4acfdfa533fe
tobiforsdyke <tobiforsdyke@gmail.com>
2020-02-18 17:27:04 +0000

    Revert "Added the change name functionality that seems pointless"
    This reverts commit 95d34ec9827d380eb663af1a68b919701b7c4fe8.

--- popup.js
+++ popup.js
@@ -1,5 +1,4 @@
-let clickmeBTN = document.getElementById('clickmeBTN');
-let extensionNameH1 = document.getElementById('extensionNameH1');
+let clickmeBTN = document.getElementById('clickmeBTN')
 
 var changeColour = function(colour){
   chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
@@ -10,16 +9,7 @@ var changeColour = function(colour){
   });
 };
 
+
 clickmeBTN.addEventListener('click', function(e){
   changeColour('blue');
 });
-
-function load_extension_name(){
-  chrome.storage.sync.get(['extensionText'], function(result) {
-    if(result.extensionText != undefined){
-      extensionNameH1.innerHTML = result.extensionText;
-    }
-  });
-}
-
-document.addEventListener('DOMContentLoaded',load_extension_name);
